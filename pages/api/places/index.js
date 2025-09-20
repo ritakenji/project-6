84e694d36a9fa871ea4a6fc64d98c874aeb3cd64
Rita <rita.kenji@gmail.com>
2025-09-20 11:34:53 +0200

    add POST method to pages/api/index and fetch method POST on create.js and router push user to homepage

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,9 +4,24 @@ import Place from "@/db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const places = await Place.find();
-    response.status(200).json(places);
+  try {
+    if (request.method === "GET") {
+      const places = await Place.find(); //Find them all
+      response.status(200).json(places);
+      return;
+    }
+
+    //Where do the new places need to go? pages/api/places/index, why not [id]? Because the new place won't have a new id when submitted
+
+    if (request.method === "POST") {
+      const placeData = request.body; //Where can the FE send info on the new place to the BE? Int he body of the request
+      await Place.create(placeData); //create aka add for POST method in BE
+      response.status(200).json({ status: "Place created." });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal Server Error" });
     return;
   }
 
--- pages/create.js
+++ pages/create.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form";
 import { StyledLink } from "../components/StyledLink";
-import useSWR from "swr";
+/* import useSWR from "swr"; */
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
@@ -10,9 +10,41 @@ const StyledBackLink = styled(StyledLink)`
 
 export default function CreatePlacePage() {
   const router = useRouter();
+  /* const { mutate } = useSWR("/api/places"); */
+
   async function addPlace(place) {
-    console.log("adding place");
+    // I need to send the info from the form to the BKEND
+    console.log("adding place", place);
+
+    const response = await fetch("api/places", {
+      //need to sent this newPlace to the BKEND to b save in the DB
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(place),
+    });
+
+    if (response.ok) {
+      /* mutate(); //once i know the responsde is OK, I call mutate method of useSWR to re-render page */
+      router.push("/"); // using push instead because we dont want to rerender the create page, we want to direct the user to homepage
+    }
   }
+
+  /*   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "external-content.duckduckgo.com",
+        port: "",
+      },
+    ],
+  }, */
 
   return (
     <>
